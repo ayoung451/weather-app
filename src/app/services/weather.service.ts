3dcd5ef0bc6c87786f5a0f8c7ef61dc787e548fc
Aubry <cobrajayne@gmail.com>
2022-11-04 22:37:04 -0400

    implement call to rapidApi for weather data

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,7 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +13,13 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
-  getWeatherData(cityName: string) {
-    this.http.get(environment.weatherApiBaseUrl, {
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(environment.weatherApiBaseUrl, {
       headers: new HttpHeaders()
       .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
       .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
       params: new HttpParams()
-      .set('city', cityName)
+      .set('q', cityName)
     } )
   }
 
